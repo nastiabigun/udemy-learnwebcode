9fdf4c71b720097f16647984256039c0226562e5
iNastia <nastia.bigun@gmail.com>
2018-09-05 11:52:57 -0600

    completed sticky header

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,7 +1,10 @@
 import MobileMenu from './modules/mobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import $ from 'jquery';
+import StickyHeader from './modules/StickyHeader';
 
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($(".feature-item"), "90%");
 new RevealOnScroll($(".testimonial"), "70%");
+
+var stickyHeader = new StickyHeader();

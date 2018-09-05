24674c28a7e8e4bbaafdac6fbeee32ab157eb0b2
iNastia <nastia.bigun@gmail.com>
2018-09-04 20:36:14 -0600

    completed reveal on scroll behavior

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,3 +1,7 @@
 import MobileMenu from './modules/mobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
+import $ from 'jquery';
 
 var mobileMenu = new MobileMenu();
+new RevealOnScroll($(".feature-item"), "90%");
+new RevealOnScroll($(".testimonial"), "70%");
